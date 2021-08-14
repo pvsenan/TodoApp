20d33bbe6fbb4a46593b76ccd2f32af1149ed6bd
Prabhendu Senan <pvsenan@gmail.com>
2021-08-14 22:49:04 +0200

    Implement logging

--- src/main/java/com/pscode/todoapp/service/TodoService.java
+++ src/main/java/com/pscode/todoapp/service/TodoService.java
@@ -2,6 +2,8 @@ package com.pscode.todoapp.service;
 
 import com.pscode.todoapp.repository.TodoRepository;
 import com.pscode.todoapp.model.TaskModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -11,26 +13,28 @@ import java.util.Optional;
 @Service
 public class TodoService {
     private final TodoRepository todoRepository;
+    Logger logger = LoggerFactory.getLogger(TodoService.class);
 
     public TodoService(TodoRepository todoRepository) {
         this.todoRepository = todoRepository;
     }
 
     public Optional<TaskModel> createTodo(TaskModel newTodoTask) {
+        logger.info("Creating new todo");
         return Optional.of(todoRepository.save(newTodoTask));
     }
 
-    public Optional<TaskModel> editExistingTodo(TaskModel updatedTodoTask, Long todoId){
+    public Optional<TaskModel> editExistingTodo(TaskModel updatedTodoTask, Long todoId) {
         Optional<TaskModel> todoTask = todoRepository.findById(todoId);
-        if(todoTask.isPresent()){
+        if (todoTask.isPresent()) {
             TaskModel model = todoTask.get();
             model.setTodoName(updatedTodoTask.getTodoName());
             model.setCompleted(updatedTodoTask.getCompleted());
             model.setCreatedAt(LocalDateTime.now());
             todoRepository.save(model);
             return Optional.of(model);
-        }else{
-            // Add logging
+        } else {
+            logger.error("Failed to update todo with id {}", todoId);
             return Optional.empty();
         }
     }
