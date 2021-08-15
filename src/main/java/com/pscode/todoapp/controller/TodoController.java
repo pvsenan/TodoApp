af9b26e4cf1445ce30c70ee411ac0e20dfbd43d9
Prabhendu Senan <pvsenan@gmail.com>
2021-08-15 09:09:07 +0200

    implement GET and DELETE oprations

--- src/main/java/com/pscode/todoapp/controller/TodoController.java
+++ src/main/java/com/pscode/todoapp/controller/TodoController.java
@@ -1,5 +1,6 @@
 package com.pscode.todoapp.controller;
 
+import antlr.StringUtils;
 import com.pscode.todoapp.model.TaskModel;
 import com.pscode.todoapp.service.TodoService;
 import org.springframework.http.HttpStatus;
@@ -7,6 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.security.PublicKey;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -35,6 +38,37 @@ public class TodoController {
             return ResponseEntity.ok(result.get());
         }else{
             return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<?> getAllTodos(){
+        Optional<List<TaskModel>> todo = todoService.getAll();
+        if(todo.isPresent()){
+            return ResponseEntity.ok(todo.get());
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<?> getOneTodo(@PathVariable Long id){
+        Optional<TaskModel> todo = todoService.getOne(id);
+        if(todo.isPresent()){
+            return ResponseEntity.ok(todo.get());
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteTodo(@PathVariable Long id){
+        Optional<TaskModel> todo = todoService.delete(id);
+        if(todo.isPresent()){
+            return ResponseEntity.ok(todo.get());
+        }else{
+            String error = String.format("Unable to find todo with id %d. Please make sure that the id is correct", id);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
         }
     }
 }
